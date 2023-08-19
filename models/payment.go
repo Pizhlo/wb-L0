15f8e42a99f4baee6ba7dc341719c70c75124416
pizhlo <zaush@bk.ru>
2023-08-19 18:26:10 +0300

    changed type in payments.request_id

--- models/payment.go
+++ models/payment.go
@@ -1,14 +1,17 @@
 package models
 
+import "database/sql"
+
 type Payment struct {
-	Transaction  string `json:"transaction"`
-	RequestID    string `json:"request_id"`
-	Currency     string `json:"currency"`
-	Provider     string `json:"provider"`
-	Amount       int    `json:"amount"`
-	PaymentDate  string `json:"payment_dt"`
-	Bank         string `json:"bank"`
-	DeliveryCost int    `json:"delivery_cost"`
-	GoodsTotal   int    `json:"goods_total"`
-	CustomFee    int    `json:"custom_fee"`
+	ID           int
+	Transaction  string         `json:"transaction"`
+	RequestID    sql.NullString `json:"request_id"`
+	Currency     string         `json:"currency"`
+	Provider     string         `json:"provider"`
+	Amount       int            `json:"amount"`
+	PaymentDate  string         `json:"payment_dt"`
+	Bank         string         `json:"bank"`
+	DeliveryCost int            `json:"delivery_cost"`
+	GoodsTotal   int            `json:"goods_total"`
+	CustomFee    int            `json:"custom_fee"`
 }
