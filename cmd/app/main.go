a81d23a485444f3f2533faee1a58c8de86a61bf0
pizhlo <zaush@bk.ru>
2023-08-19 18:25:10 +0300

    implemented server; added router

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,23 +1,42 @@
 package main
 
 import (
+	"log"
 	"net/http"
 
 	"github.com/go-chi/chi"
 
 	handlers "github.com/Pizhlo/wb-L0/internal/app/handlers"
+	storage "github.com/Pizhlo/wb-L0/internal/app/storage/postgres"
+	"github.com/Pizhlo/wb-L0/service"
 )
 
 func main() {
-	//conf := ParseFlags()
-	
-	run()
+	conf := ParseFlags()
+
+	conn, err := storage.Connect(conf.FlagDBAddress)
+	if err != nil {
+		log.Fatal("unable to connect db: ", err)
+	}
+
+	db, err := storage.New(conn)
+	if err != nil {
+		log.Fatal("unable to create db: ", err)
+	}
+
+	service := service.New(db)
+
+	if err := http.ListenAndServe(":8080", run(service)); err != nil {
+		log.Fatal("error while executing server: ", err)
+	}
 }
 
-func run() {
+func run(service *service.Service) chi.Router {
 	r := chi.NewRouter()
 
 	r.Get("/{id}", func(w http.ResponseWriter, r *http.Request) {
-		handlers.GetByID(w, r)
+		handlers.GetByID(w, r, *service)
 	})
+
+	return r
 }
