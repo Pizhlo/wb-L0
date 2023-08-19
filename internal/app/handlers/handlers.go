ef06c49b30fe47d72abf684187b5b6e17f630953
pizhlo <zaush@bk.ru>
2023-08-19 18:36:29 +0300

    added handler get by id

--- internal/app/handlers/handlers.go
+++ internal/app/handlers/handlers.go
@@ -1,7 +1,37 @@
 package internal
 
-import "net/http"
+import (
+	"encoding/json"
+	"net/http"
 
-func GetByID(w http.ResponseWriter, r *http.Request){
-	
+	"log"
+
+	"github.com/Pizhlo/wb-L0/service"
+	"github.com/go-chi/chi"
+	"github.com/google/uuid"
+)
+
+func GetByID(w http.ResponseWriter, r *http.Request, service service.Service) {
+	id := chi.URLParam(r, "id")
+
+	uuid, err := uuid.Parse(id)
+	if err != nil {
+		log.Default().Printf("uuid %s is invalid: %s", id, err)
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	order, err := service.Storage.GetOrderByID(r.Context(), uuid)
+	if err != nil {
+		log.Default().Println("error while getting order by ID: ", err)
+	}
+
+	orderJson, err := json.Marshal(order)
+	if err != nil {
+		log.Default().Println("error while making json: ", err)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+
+	w.Write(orderJson)
 }
