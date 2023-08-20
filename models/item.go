e6e6d8ec788822caeac8473e41a8208cce718c3a
pizhlo <zaush@bk.ru>
2023-08-20 18:40:08 +0300

    updated field types

--- models/item.go
+++ models/item.go
@@ -1,15 +1,18 @@
 package models
 
+import uuid "github.com/google/uuid"
+
 type Item struct {
-	ChrtId      int    `json:"chrt_id"`
-	TrackNumber string `json:"track_number"`
-	Price       int    `json:"price"`
-	RID         string `json:"rid"`
-	Name        string `json:"name"`
-	Sale        int    `json:"sale"`
-	Size        string `json:"size"`
-	TotalPrice  int    `json:"total_price"`
-	NmID        int    `json:"nm_id"`
-	Brand       string `json:"brand"`
-	Status      int    `json:"status"`
+	ID          int
+	ChrtId      int       `json:"chrt_id"`
+	TrackNumber string    `json:"track_number"`
+	Price       int       `json:"price"`
+	RID         uuid.UUID `json:"rid"`
+	Name        string    `json:"name"`
+	Sale        int       `json:"sale"`
+	Size        string    `json:"size"`
+	TotalPrice  int       `json:"total_price"`
+	NmID        int       `json:"nm_id"`
+	Brand       string    `json:"brand"`
+	Status      int       `json:"status"`
 }
--- models/payment.go
+++ models/payment.go
@@ -1,15 +1,19 @@
 package models
 
-import "database/sql"
+import (
+	"database/sql"
+
+	uuid "github.com/google/uuid"
+)
 
 type Payment struct {
 	ID           int
-	Transaction  string         `json:"transaction"`
+	Transaction  uuid.UUID      `json:"transaction"`
 	RequestID    sql.NullString `json:"request_id"`
 	Currency     string         `json:"currency"`
 	Provider     string         `json:"provider"`
 	Amount       int            `json:"amount"`
-	PaymentDate  string         `json:"payment_dt"`
+	PaymentDate  int64          `json:"payment_dt"`
 	Bank         string         `json:"bank"`
 	DeliveryCost int            `json:"delivery_cost"`
 	GoodsTotal   int            `json:"goods_total"`
