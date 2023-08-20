d9c0566054cec8b0d9df1fb71851c1b91a3770ef
pizhlo <zaush@bk.ru>
2023-08-20 18:41:03 +0300

    added subscriber

---
+++ internal/stream/subscriber/subscriber.go
@@ -0,0 +1,40 @@
+package subscriber
+
+import (
+	"context"
+
+	"github.com/Pizhlo/wb-L0/internal/app/storage/postgres"
+	"github.com/Pizhlo/wb-L0/internal/stream/publisher"
+	"github.com/nats-io/nats.go"
+)
+
+type Subscribe interface {
+}
+
+type Subscriber struct {
+	*nats.EncodedConn
+}
+
+func New(c *nats.EncodedConn, storage postgres.Storage) *Subscriber {
+	subscriber := &Subscriber{c}
+
+	c.Subscribe("foo", func(msg *publisher.Msg) {
+		subscriber.ReceiveMsg(msg, storage)
+	})
+
+	return subscriber
+
+}
+
+func (s *Subscriber) ReceiveMsg(m *publisher.Msg, storage postgres.Storage) error {
+	ctx, cancel := context.WithCancel(context.TODO())
+	defer cancel()
+
+	err := storage.SaveOrder(ctx, m.Order)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
