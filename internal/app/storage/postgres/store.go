0e05d24da7620384fa9d83d0419bb0072bb292bc
pizhlo <zaush@bk.ru>
2023-08-20 18:43:41 +0300

    added new functions for saving delivery, items, payments, orders

--- internal/app/storage/postgres/store.go
+++ internal/app/storage/postgres/store.go
@@ -3,6 +3,7 @@ package postgres
 import (
 	"context"
 	"errors"
+	"fmt"
 
 	"github.com/Pizhlo/wb-L0/errs"
 	"github.com/Pizhlo/wb-L0/models"
@@ -14,8 +15,9 @@ import (
 )
 
 type Storage interface {
-	CreateTable() error
-	GetOrderByID(ctx context.Context, id int) (models.Order, error)
+	GetOrderByID(ctx context.Context, id uuid.UUID) (models.Order, error)
+	SaveOrder(ctx context.Context, order models.Order) error
+	SaveItem(ctx context.Context, items []models.Item) error
 }
 
 type DB struct {
@@ -143,3 +145,91 @@ func (db *DB) getPaymentByOrderID(ctx context.Context, orderId uuid.UUID) (model
 
 	return payment, nil
 }
+
+func (db *DB) SaveOrder(ctx context.Context, order models.Order) error {
+	fmt.Println("starting saving order...")
+	deliveryID, err := db.saveDelivery(ctx, order.Delivery)
+	if err != nil {
+		return err
+	}
+
+	paymentID, err := db.savePayment(ctx, order.Payment)
+	if err != nil {
+		return err
+	}
+
+	err = db.SaveItem(ctx, order.Items)
+	if err != nil {
+		return err
+	}
+
+	q := `INSERT INTO orders (order_id, track_number, entry, delivery_id, payment_id, locale, internal_signature, customer_id, delivery_service, shard_key, sm_id, date_created, oof_shard)
+	VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13)`
+
+	_, err = db.Exec(ctx, q, order.OrderUIID, order.TrackNumber, order.Entry, deliveryID, paymentID, order.Locale, order.InternalSignature, order.CustomerID, order.DeliveryService,
+	order.ShardKey, order.SmID, order.DateCreated, order.OofShard)
+	if err != nil {
+		fmt.Println("an error accured while saving order: ", err)
+		return err
+	}
+
+	fmt.Println("order saved successfully")
+	return nil
+}
+
+// works
+func (db *DB) saveDelivery(ctx context.Context, delivery models.Delivery) (int, error)  {
+	fmt.Println("starting saving delivery...")
+	q := `INSERT INTO delivery (name, phone, zip, city, address, region, email) VALUES($1, $2, $3, $4, $5, $6, $7)
+	RETURNING id`
+
+	row := db.QueryRow(ctx, q, delivery.Name, delivery.Phone, delivery.Zip, delivery.City, delivery.Address, delivery.Region, delivery.Email)
+	var id int
+
+	err := row.Scan(&id)
+	if err != nil {
+		return 0, err
+	}
+
+	fmt.Println("delivery saved successfully")
+
+	return id, nil
+}
+
+// works
+func (db *DB) savePayment(ctx context.Context, payment models.Payment) (int, error) {
+	fmt.Println("starting saving payment...")
+
+	q := `INSERT INTO payments (transaction, request_id, currency, provider, amount, payment_date, bank, delivery_cost, goods_total, custom_fee) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10) RETURNING id`
+
+	var id int 
+
+	row := db.QueryRow(ctx, q, payment.Transaction, payment.RequestID, payment.Currency, payment.Provider, payment.Amount, payment.PaymentDate, payment.Bank, payment.DeliveryCost, payment.GoodsTotal, payment.CustomFee)
+	err := row.Scan(&id)
+	if err != nil {
+		return 0, err
+	}
+
+	fmt.Println("payment saved successfully")
+	return id, nil
+}
+
+func (db *DB) SaveItem(ctx context.Context, items []models.Item) error {
+	fmt.Println("starting saving items...")
+
+	//fmt.Printf("got these items: %+v\n", items)
+
+	q := `INSERT INTO items (chrt_id, track_number, price, rid, name, sale, size, total_price, nm_id, brand, status) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11)`
+
+	for _, item := range items {
+		_, err := db.Exec(ctx, q, item.ChrtId, item.TrackNumber, item.Price, item.RID, item.Name, item.Sale, item.Size, item.TotalPrice, item.NmID, item.Brand, item.Status)
+		if err != nil {
+			fmt.Println("an error accured while saving item: ", err)
+			return err
+		}
+	}
+
+	fmt.Println("items saved successfully")
+	return nil
+}
+
