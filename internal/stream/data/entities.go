e30248cda9879d19bee27e255ede6dacc721113c
pizhlo <zaush@bk.ru>
2023-08-20 18:41:34 +0300

    added scripts for generating random msg for streaming

---
+++ internal/stream/data/entities.go
@@ -0,0 +1,96 @@
+package data
+
+import (
+	"database/sql"
+	"fmt"
+
+	"github.com/Pizhlo/wb-L0/models"
+)
+
+func RandomOrder() models.Order {
+	entryEnum := []string{"WBIL"}
+
+	item, trackNumber := randomItem()
+
+	order := models.Order{
+		ID:          randomInt(0, 50),
+		OrderUIID:   randomUIID(),
+		TrackNumber: trackNumber,
+		Entry:       randomChoise(entryEnum),
+		Delivery:    randomDelivery(),
+		Payment:     randomPayment(),
+		Items:       []models.Item{item},
+		Locale:      "en",
+		InternalSignature: sql.NullString{
+			String: "",
+			Valid:  false,
+		},
+		CustomerID:      randomString(3),
+		DeliveryService: "meest",
+		ShardKey:        randomString(1),
+		SmID:            randomInt(1, 100),
+		DateCreated:     randomTime(),
+		OofShard:        randomString(1),
+	}
+
+	return order
+}
+
+func randomDelivery() models.Delivery {
+	delivery := models.Delivery{
+		Name:    randomString(10),
+		Phone:   randomPhone(),
+		Zip:     randomString(6),
+		City:    randomString(6),
+		Address: randomString(10),
+		Region:  randomString(11),
+		Email:   randomEmail(5),
+	}
+
+	return delivery
+}
+
+func randomItem() (models.Item, string) {
+	trackNumber := randomString(10)
+
+	item := models.Item{
+		ID:          randomInt(1, 50),
+		ChrtId:      randomInt(100, 10000),
+		TrackNumber: trackNumber,
+		Price:       randomInt(1000, 5000),
+		RID:         randomUIID(),
+		Name:        randomString(10),
+		Sale:        randomInt(0, 51),
+		Size:        randomString(3),
+		TotalPrice:  randomInt(250, 1600),
+		NmID:        randomInt(100000, 1500000),
+		Brand:       randomString(10),
+		Status:      randomInt(100, 200),
+	}
+
+	fmt.Println("random status trackNumber = ", item.TrackNumber)
+
+	return item, trackNumber
+}
+
+func randomPayment() models.Payment {
+	currencyEnum := []string{"USD", "RUB"}
+	providerEnum := []string{"wbpay"}
+
+	payment := models.Payment{
+		ID:          randomInt(1, 20),
+		Transaction: randomUIID(),
+		RequestID: sql.NullString{
+			Valid: false,
+		},
+		Currency:     randomChoise(currencyEnum),
+		Provider:     randomChoise(providerEnum),
+		PaymentDate:  randomTimeISO(),
+		Bank:         "alpha",
+		DeliveryCost: randomInt(700, 1500),
+		GoodsTotal:   randomInt(200, 1000),
+		CustomFee:    0,
+	}
+
+	return payment
+}
---
+++ internal/stream/data/random.go
@@ -0,0 +1,68 @@
+package data
+
+import (
+	"math/rand"
+	"strconv"
+	"time"
+
+	"github.com/google/uuid"
+)
+
+const charset = "abcdefghijklmnopqrstuvwxyz" +
+	"ABCDEFGHIJKLMNOPQRSTUVWXYZ01234567890123456789"
+
+func randomInt(min, max int) int {
+	return min + rand.Intn(max-min+1)
+}
+
+func randomString(length int) string {
+	b := make([]byte, length)
+	source := rand.New(
+		rand.NewSource(time.Now().UnixNano()))
+	r1 := rand.New(source)
+
+	for i := range b {
+		b[i] = charset[r1.Intn(len(charset))]
+	}
+	return string(b)
+}
+
+func randomUIID() uuid.UUID {
+	return uuid.New()
+}
+
+func randomTime() time.Time {
+	return time.Now()
+}
+
+func randomTimeISO() int64 {
+	return time.Now().Unix()
+}
+
+func randomChoise(slice []string) string {
+	source := rand.New(
+		rand.NewSource(time.Now().UnixNano()))
+	r1 := rand.New(source)
+
+	n := r1.Int() % len(slice)
+
+	return slice[n]
+}
+
+func randomPhone() string {
+	phoneStr := "+7"
+
+	phone := randomInt(9000000000, 9999999999)
+
+	phoneStr += strconv.Itoa(phone)
+
+	return phoneStr
+}
+
+func randomEmail(n int) string {
+	login := randomString(n)
+
+	login += "@mail.ru"
+
+	return login
+}
