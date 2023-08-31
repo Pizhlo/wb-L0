47fab9d42bdfadbbfa0dbfd7ac78c87a7a1e6a81
Maria <zaush@bk.ru>
2023-08-31 16:57:58 +0300

    implemetned cache in service

--- internal/service/service.go
+++ internal/service/service.go
@@ -1,15 +1,18 @@
 package service
 
 import (
+	"github.com/Pizhlo/wb-L0/internal/app/storage/cache"
 	"github.com/Pizhlo/wb-L0/internal/app/storage/postgres"
 )
 
 type Service struct {
-	Repo postgres.Repo
+	Repo  postgres.Repo
+	Cache *cache.Cache
 }
 
-func New(repo postgres.Repo) *Service {
+func New(repo postgres.Repo, cache *cache.Cache) *Service {
 	return &Service{
-		Repo: repo,
+		Repo:  repo,
+		Cache: cache,
 	}
 }
