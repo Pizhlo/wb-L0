cfb6b0be57345b0e5e6efaed32ba551994640a6f
Maria <zaush@bk.ru>
2023-09-01 18:06:24 +0300

    changes sql nullstring to *string

--- internal/model/payment.go
+++ internal/model/payment.go
@@ -1,21 +1,19 @@
 package models
 
 import (
-	"database/sql"
-
 	uuid "github.com/google/uuid"
 )
 
 type Payment struct {
 	ID           int
-	Transaction  uuid.UUID      `json:"transaction"`
-	RequestID    sql.NullString `json:"request_id"`
-	Currency     string         `json:"currency"`
-	Provider     string         `json:"provider"`
-	Amount       int            `json:"amount"`
-	PaymentDate  int64          `json:"payment_dt"`
-	Bank         string         `json:"bank"`
-	DeliveryCost int            `json:"delivery_cost"`
-	GoodsTotal   int            `json:"goods_total"`
-	CustomFee    int            `json:"custom_fee"`
+	Transaction  uuid.UUID `json:"transaction"`
+	RequestID    *string   `json:"request_id"`
+	Currency     string    `json:"currency"`
+	Provider     string    `json:"provider"`
+	Amount       int       `json:"amount"`
+	PaymentDate  int64     `json:"payment_dt"`
+	Bank         string    `json:"bank"`
+	DeliveryCost int       `json:"delivery_cost"`
+	GoodsTotal   int       `json:"goods_total"`
+	CustomFee    int       `json:"custom_fee"`
 }
